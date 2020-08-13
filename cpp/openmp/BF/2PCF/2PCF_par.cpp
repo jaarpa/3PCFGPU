c4edf668f5b0b11a84311fae3f1a4e508569f2c1
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-08-13 16:00:52 -0500

    2PCF isotropica paralelizado correctamente

--- cpp/openmp/BF/2PCF/2PCF_par.cpp
+++ cpp/openmp/BF/2PCF/2PCF_par.cpp
@@ -33,7 +33,7 @@ void read_file(string file_loc, Punto *data){
     }
 }
 
-void guardar_Histograma(string nombre,int dim, float*histograma){
+void guardar_Histograma(string nombre,int dim, int*histograma){
     ofstream archivo;
     archivo.open(nombre.c_str(),ios::out | ios::binary);
     if (archivo.fail()){
@@ -53,69 +53,6 @@ float distance(Punto p1, Punto p2){
 }
 
 /*
-void histogramasPuros(float *DD, float *RR, flat){
-    int i,j, pos;
-    float dd, rr, ds = (float)(num_bins), aux;
-    ds /= d_max;
-    printf("\n -- ds = %f -- \n", ds);
-    for (i = 0; i < num_puntos-1; i++)
-    {
-        for (j = i+1; j < num_puntos; j++)
-        {
-            // Metodo aprox
-            //aux = distancia(data[i].x - data[j].x, data[i].y - data[j].y);
-            //dd = distancia(aux, data[i].z - data[j].z);
-            //aux = distancia(rand[i].x - rand[j].x, rand[i].y - rand[j].y);
-            //rr = distancia(aux, rand[i].z - rand[j].z);
-
-            // Distancia euclidea
-            //dd = dist(data[i].x-data[j].x, data[i].y - data[j].y, data[i].z - data[j].z);
-            //rr = dist(rand[i].x-rand[j].x, rand[i].y - rand[j].y, rand[i].z - rand[j].z);
-
-            //2D
-            dd = euclidean_distance2D(data[i].x - data[j].x, data[i].y - data[j].y);
-            rr = euclidean_distance2D(rand[i].x - rand[j].x, rand[i].y - rand[j].y);
-            if (dd < d_max)
-            {
-                pos = (int)(dd*ds);
-                DD[pos] += 2;
-            }
-            if (rr < d_max)
-            {
-                pos = (int)(rr*ds);
-                RR[pos] += 2;
-            }   
-        }
-    }
-}
-void histogramasMixtos(float *DR){
-    int i,j,pos;
-    float dr, s, aux;
-    s = (float)(num_bins)/d_max;
-    for (i = 0; i < num_puntos; i++)
-    {
-        for (j = 0; j < num_puntos; j++)
-        {
-            // Método aprox
-            //aux = distancia(data[i].x - rand[j].x, data[i].y - rand[j].y);
-            //dr = distancia(aux, data[i].z - rand[j].z);
-
-            // Distancia euclidea
-           // dr = dist(data[i].x - rand[j].x, data[i].y - rand[j].y, data[i].z - rand[j].z);
-
-           //2D
-           dr = euclidean_distance2D(data[i].x - rand[j].x, data[i].y - rand[j].y);
-            if (dr < d_max)
-            {
-                pos = (int)(dr*s);
-                DR[pos] += 1;
-            }
-        }
-    }
-}
-*/
-
-/*
 Parameters:
 1 Data file name
 2 Random file name
@@ -124,12 +61,7 @@ Parameters:
 5 Distancia maxima
 */
 int main(int argc, char **argv){
-    //string data_loc = argv[1];
-    //string rand_loc = argv[2];
     string mypathto_files = "../../../../fake_DATA/DATOS/";
-    //This creates the full path to where I have my data files
-    //data_loc.insert(0,mypathto_files);
-    //rand_loc.insert(0,mypathto_files);
     
     unsigned int N = stoi(argv[3]), bins=stoi(argv[4]);
     float d_max=stof(argv[5]);
@@ -137,8 +69,6 @@ int main(int argc, char **argv){
     Punto *rand = new Punto[N]; //Crea un array de N puntos
 
     //Llama a una funcion que lee los puntos y los guarda en la memoria asignada a data y rand
-
-    // Podria hacer dos threads y que cada una lea un archivo.
     
     #pragma omp parallel num_threads(2)
     {
@@ -172,65 +102,79 @@ int main(int argc, char **argv){
     */
 
     // Crea los histogramas
-    float *DD, *DR, *RR;
-    DD = new float[bins];
-    DR = new float[bins];
-    RR = new float[bins];
-    //Inicializa en 0
-    for (int i=0; i<bins; i++){
-        DD[i] = 0.0, RR[i] = 0.0, DR[i] = 0.0;     
-    }
-    double dbin = (double)bins/d_max;
-    cout << dbin << endl;
-    float d;
-    //Hace el conteo para el histograma DD
+    int *DD, *DR, *RR;
+    DD = new int[bins];
+    omp_lock_t DD_lock[bins];
+    DR = new int[bins];
+    omp_lock_t DR_lock[bins];
+    RR = new int[bins];
+    omp_lock_t RR_lock[bins];
     
-    #pragma omp parallel
-    {
-	    int ID = omp_get_thread_num(), threads = omp_get_num_threads(), histo_idx; //Get the thread number
-	    //Sub hisogram from each thread, at the end Ill sum up all of them
-        
-	    float *sub_DD, *sub_RR;
-	    sub_DD = new float[bins];
-	    sub_RR = new float[bins];
-	    
-	    for (int i=0; i<bins; i++){
-		sub_DD[i] = 0.0, sub_RR[i] = 0.0;
-	    }
-	    
-	    for (int i=ID; i<N-1; i=i+threads){
-		    for (int j=i+1; j<N; j++){
-		        d = distance(data[i],data[j]);
-		        if (d<=d_max){
-    		        histo_idx = (int)(d*dbin);
-    		    }
-		        
-		        //if (t_norm<=d_max){
-		            //sub_DD[(int)(t_norm/dbin)]+=2;
-		        //}
-		        
-		        d = distance(rand[i],rand[j]);
-		        if (d<=d_max){
-		            cout << d << endl;
-    		        histo_idx = (int)(d*dbin);
-    		        if (histo_idx >= 30){
-    		            cout << "( " << i << ", " << j << " )" << endl;
-    		            cout << histo_idx << endl;
-    		            
-    		        }
-    		    }
-		        
-		        //if (t_norm<=d_max){
-		            //sub_RR[(int)(t_norm/dbin)]+=2;
-		        //}
-		    }
-	    }
+    //omp_init_lock()
+    //omp_set_lock()
+    //omp_unset_lock()
+    //omp_destroy_lock()
+    //omp_test_lock()
+    
+    //Inicializa en 0 y bloquea cada bin en el histograma
+    
+    #pragma openmp parallel for
+    for (int i=0; i<bins; i++){
+        omp_init_lock(&DD_lock[i]),omp_init_lock(&RR_lock[i]),omp_init_lock(&DR_lock[i]);
+        DD[i] = 0, RR[i] = 0, DR[i] = 0;
+    }
+    
+    
+    double dbin = (double)bins/d_max;
+    
+    //Hace el conteo para el histograma DD
+    #pragma openmp parallel for
+    for (int i=0; i<N-1; i++){
+        float d;
+        int idh;
+        for (int j=i+1; j<N; j++){
+            d = distance(data[i],data[j]);
+            if (d<=d_max){
+                idh = (int)(d*dbin);
+                omp_set_lock(&DD_lock[idh]);
+                    DD[(int)(d*dbin)]+=2;
+                omp_unset_lock(&DD_lock[idh]);
+            }
+	        d = distance(rand[i],rand[j]);
+            if (d<=d_max){
+                idh = (int)(d*dbin);
+                omp_set_lock(&RR_lock[idh]);
+                    RR[(int)(d*dbin)]+=2;
+                omp_unset_lock(&RR_lock[idh]);
+            }
+        }
+    }
+    
+    //Hace el conteo para el histograma DR
+    #pragma openmp parallel for
+    for (int i=0; i<N; i++){
+        float d;
+        int idh;
+        for (int j=0; j<N; j++){
+            d = distance(rand[i],data[j]);
+            if (d<=d_max){
+                idh = (int)(d*dbin);
+                omp_set_lock(&DR_lock[idh]);
+                    DR[(int)(d*dbin)]+=1;
+                omp_unset_lock(&DR_lock[idh]);
+            }
+        }
+    }
+    
+    //Destruye los bloqueos
+    for (int i=0; i<bins; i++){
+    	omp_destroy_lock(&DD_lock[i]),omp_destroy_lock(&RR_lock[i]),omp_destroy_lock(&DR_lock[i]);
     }
     
     // Guarda los histogramas
-    guardar_Histograma("DD.dat", bins, DD);
-    guardar_Histograma("RR.dat", bins, RR);
-    guardar_Histograma("DR.dat", bins, DR);
+    guardar_Histograma("DD_par.dat", bins, DD);
+    guardar_Histograma("RR_par.dat", bins, RR);
+    guardar_Histograma("DR_par.dat", bins, DR);
     
     return 0;
 }
