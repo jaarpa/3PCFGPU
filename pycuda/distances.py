7be2e1ac5bb1bfe493829140e7fee6821c916663
Josue De Santiago <joselotl@gmail.com>
2020-08-24 02:10:40 -0500

    Added perfilation points

--- pycuda/distances.py
+++ pycuda/distances.py
@@ -25,7 +25,6 @@ __global__ void XY(float *dest, float *a, float *b)
    float d;
    //float histo[30];
    int bin;
-
    d = sqrt(pow(a[p_x] - b[x],2)+pow(a[p_y]-b[y],2) + pow(a[p_z]-b[z],2));
    if (d<=180){
       bin = (int) (d/6.0);
@@ -64,11 +63,13 @@ XX = mod.get_function("XX")
 data = np.loadtxt(fname='../fake_DATA/DATOS/data_500.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
 rand = np.loadtxt(fname='../fake_DATA/DATOS/rand0_500.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
 
+t = 0.0
 dest = np.zeros_like(data[:30,0])
 start = time.perf_counter()
 XY(drv.Out(dest), drv.In(data), drv.In(rand), block=(500,1,1), grid=(500,1))
 end = time.perf_counter()
 print(f'DR {end-start}s in GPU')
+t+=(end-start)
 np.savetxt('DR.dat', dest)
 
 dest = np.zeros_like(data[:30,0])
@@ -77,6 +78,7 @@ XX(drv.Out(dest), drv.In(data), block=(500,1,1), grid = (500,1))
 end = time.perf_counter()
 print(f'DD {end-start}s in GPU')
 np.savetxt('DD.dat', dest)
+t+=(end-start)
 
 dest = np.zeros_like(data[:30,0])
 start = time.perf_counter()
@@ -84,6 +86,9 @@ XX(drv.Out(dest), drv.In(rand), block=(500,1,1), grid = (500,1))
 end = time.perf_counter()
 print(f'RR {end-start}s in GPU')
 np.savetxt('RR.dat', dest)
+t+=(end-start)
+
+print(f'Total time {t} s')
 
 
 start = time.perf_counter()
@@ -92,10 +97,8 @@ for i,point in enumerate(rand):
     test_r = np.sqrt(np.sum((point-rand[i:])**2,axis=1))
     t_count, edges = np.histogram(test_r, bins=30, range=(0,180))
     count += t_count
+count*=2
 end = time.perf_counter()
 print(f'{end-start} s in CPU')
 
-print(test_r[:20])
-print(count)
-print(dest)
-print(np.abs(dest-count)<=1e-5)
+print(np.abs(dest-count)<=1e-10)
