6cfc78dc1a71d255712eff9377e137b8de91c570
Josue De Santiago <joselotl@gmail.com>
2020-08-24 21:38:51 -0500

    Succesfully worked parrellized for the whole box BF 2isotropic

--- pycuda/distances.py
+++ pycuda/distances.py
@@ -37,7 +37,7 @@ mod = SourceModule("""
         int p_id = threadIdx.x + blockDim.x*blockIdx.x;
         int id = threadIdx.y + blockDim.y*blockIdx.y;
 
-        if (p_id<*N && id<*N && p_id<=id){
+        if (p_id<*N && id<*N && p_id<id){
 
             int p_x = p_id*3;
             int p_y = p_x+1;
@@ -62,24 +62,27 @@ mod = SourceModule("""
 XY = mod.get_function("XY")
 XX = mod.get_function("XX")
 
-data = np.loadtxt(fname='../fake_DATA/DATOS/data_500.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
-rand = np.loadtxt(fname='../fake_DATA/DATOS/rand0_500.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
+data = np.loadtxt(fname='../fake_DATA/DATOS/data.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
+rand = np.loadtxt(fname='../fake_DATA/DATOS/rand0.dat', delimiter=" ", usecols=(0,1,2)).astype(np.float32)
 
 N = len(data)
 
 N_even = N+(N%2 != 0) #Converts N into an even number.
 temp = []
 
-for i in range(1,11):
+for i in range(1,6):
     threads = 2**i
     blocks = int(N_even/threads)+1
     unused_cores = (blocks*threads)-N_even
-    temp += [(threads,blocks,unused_cores, blocks*unused_cores)]
+    temp += [(threads,blocks,unused_cores, (blocks)**2+unused_cores**2)]
 
 temp.sort(key=lambda tup: tup[3])
 threads, blocks, unused_cores, score = temp[0] #Gets the arrange of threads and blocks with the minimum number of blocks and unused cores
 block_dim = (threads,threads,1)
 grid_dim = (blocks,blocks,1)
+
+print(block_dim)
+print(grid_dim)
 
 #template_array = np.arange(30)
 N = np.int32(N) #Number of points
@@ -96,14 +99,14 @@ t+=(end-start)
 np.savetxt('DR.dat', DR)
 
 start = time.perf_counter()
-XX(drv.Out(DD), drv.In(data),block=block_dim, grid = grid_dim)
+XX(drv.Out(DD), drv.In(data), drv.In(N), block=block_dim, grid = grid_dim)
 end = time.perf_counter()
 print(f'DD {end-start}s in GPU')
 t+=(end-start)
 np.savetxt('DD.dat', DD)
 
 start = time.perf_counter()
-XX(drv.Out(RR), drv.In(rand), block=block_dim, grid = grid_dim)
+XX(drv.Out(RR), drv.In(rand), drv.In(N), block=block_dim, grid = grid_dim)
 end = time.perf_counter()
 print(f'RR {end-start}s in GPU')
 t+=(end-start)
@@ -111,21 +114,21 @@ np.savetxt('RR.dat', RR)
 
 print(f'Total GPU time {t}')
 
-start = time.perf_counter()
-count = np.zeros(30)
-for i,point in enumerate(rand):
-    test_r = np.sqrt(np.sum((point-rand[i:])**2,axis=1))
-    t_count, edges = np.histogram(test_r, bins=30, range=(0,180))
-    count += t_count
-end = time.perf_counter()
-print(f'{end-start} s in CPU')
+#start = time.perf_counter()
+#count = np.zeros(30)
+#for i,point in enumerate(rand):
+#    test_r = np.sqrt(np.sum((point-rand[i:])**2,axis=1))
+#    t_count, edges = np.histogram(test_r, bins=30, range=(0,180))
+#    count += t_count
+#end = time.perf_counter()
+#print(f'{end-start} s in CPU')
 
 #Compare with the backup
-RR_bak = np.loadtxt('RR_bak.dat')
-print(np.abs(RR-RR_bak)<=1e-5)
+#RR_bak = np.loadtxt('RR_bak.dat')
+#print(np.abs(RR-RR_bak)<=1e-5)
 
-DD_bak = np.loadtxt('DD_bak.dat')
-print(np.abs(DD-DD_bak)<=1e-5)
+#DD_bak = np.loadtxt('DD_bak.dat')
+#print(np.abs(DD-DD_bak)<=1e-5)
 
-DR_bak = np.loadtxt('DR_bak.dat')
-print(np.abs(DR-DR_bak)<=1e-5)
+#DR_bak = np.loadtxt('DR_bak.dat')
+#print(np.abs(DR-DR_bak)<=1e-5)
