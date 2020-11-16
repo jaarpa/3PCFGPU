a4e2b1fd2dc632b0796589c9248558d518a804ad
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-11-16 02:12:23 -0600

    Tryint to use atomicAdd with floats again

--- cuda/test_atomicAdd.cu
+++ cuda/test_atomicAdd.cu
@@ -1,40 +1,19 @@
 #include <iostream>
 
-/*
-#if __CUDA_ARCH__ < 600
-__device__ double atomicAdd(double* address, double val)
-{
-    unsigned long long int* address_as_ull =
-                              (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
 
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(val +
-                               __longlong_as_double(assumed)));
-
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
-    } while (assumed != old);
-
-    return __longlong_as_double(old);
-}
-#endif
-*/
-
-__global__ void kernel(double *XX, float *data) {
+__global__ void kernel(float *XX, float *data) {
     int bin = 0;
-    double sum = 2.4;
+    float sum = 2.4f;
     if (data[threadIdx.x]<15){
         atomicAdd(&XX[bin],sum);
     }
 }
 
 int main(){
-    double *XX;
+    float *XX;
     float *data;
     cudaMallocManaged(&data, 50*sizeof(float));
-    cudaMallocManaged(&XX, 10*sizeof(double));
+    cudaMallocManaged(&XX, 10*sizeof(float));
     for (int i=0 ; i<50 ;i++){
         data[i] = i*0.5;
     }
