66547f816342f33ae129893146a7012064ca3afe
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-11-16 13:48:15 -0600

    Syntax errors

--- cuda/2PCFisotropic/normal/dynamic.cu
+++ cuda/2PCFisotropic/normal/dynamic.cu
@@ -318,13 +318,13 @@ __global__ void make_histoXX(float *XX, Node ***nodeD, int partitions, int bn, f
 
             //Second node mobil in Z direction
             blocks = (int)(ceilf((float)(partitions-1))/32.0));
-            Z_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom)
+            Z_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom);
 
             //Second node mobil in YZ
-            YZ_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom)
+            YZ_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom);
 
             //Second node mobil in XYZ
-            XYZ_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom)
+            XYZ_direction<<<blocks,32>>>(XX, nodeD, partitions, dd_max_node, ds, dd_max, row, col, mom);
             
         }
     }
