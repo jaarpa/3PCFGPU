5233cc5e1467dcf3c2f4c8692c3326df364c82fc
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-11-16 02:14:18 -0600

    Tryint to use atomicAdd with floats again

--- cuda/test_atomicAdd.cu
+++ cuda/test_atomicAdd.cu
@@ -1,25 +1,27 @@
 #include <iostream>
 
 
-__global__ void kernel(float *XX, float *data) {
-    int bin = 0;
+__global__ void kernel(float XX, float *data) {
+    //int bin = 0;
     float sum = 2.4f;
     if (data[threadIdx.x]<15){
-        atomicAdd(&XX[bin],sum);
+        atomicAdd(&XX,sum);
     }
 }
 
 int main(){
-    float *XX;
+    float XX=0;
     float *data;
     cudaMallocManaged(&data, 50*sizeof(float));
-    cudaMallocManaged(&XX, 10*sizeof(float));
+    //cudaMallocManaged(&XX, 10*sizeof(float));
     for (int i=0 ; i<50 ;i++){
         data[i] = i*0.5;
     }
+    /*
     for (int i=0 ; i<10 ;i++){
         XX[i] = 0.0;
     }
+    */
     kernel<<<1,50>>>(XX, data);
 
     std::cout << XX[0] << std::endl;
