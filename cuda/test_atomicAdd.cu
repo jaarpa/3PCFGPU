ce6dc84192c38001d991285b413c23e449678e77
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-11-16 02:00:56 -0600

    Trying a double implementation

--- cuda/test_atomicAdd.cu
+++ cuda/test_atomicAdd.cu
@@ -1,8 +1,28 @@
 #include <iostream>
 
-__global__ void kernel(float *XX, float *data) {
+#if __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                              (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                               __longlong_as_double(assumed)));
+
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
+__global__ void kernel(double *XX, float *data) {
     int bin = 0;
-    float sum = 2.4;
+    double sum = 2.4;
     if (data[threadIdx.x]<15){
         atomicAdd(&XX[bin],sum);
     }
@@ -11,7 +31,7 @@ __global__ void kernel(float *XX, float *data) {
 int main(){
     float *XX, *data;
     cudaMallocManaged(&data, 50*sizeof(float));
-    cudaMallocManaged(&XX, 10*sizeof(float));
+    cudaMallocManaged(&XX, 10*sizeof(double));
     for (int i=0 ; i<50 ;i++){
         data[i] = i*0.5;
     }
