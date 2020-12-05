8c2f46611ff0d0974d2c2ee1c7f59867c3fdac32
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-05 14:12:46 -0600

    Comparing up to the Z loop

--- cuda/2PCFisotropic/BPC/main.cu
+++ cuda/2PCFisotropic/BPC/main.cu
@@ -108,8 +108,9 @@ int main(int argc, char **argv){
     cucheck(cudaMemcpyAsync(d_DR, DR, bn*sizeof(double), cudaMemcpyHostToDevice, streamDR));
 
     //Sets the number of partitions of the box and the size of each node
-    size_node = 2.176*(size_box/pow((float)(np),1/3.)); //ceil(size_box/(float)(partitions));
-    partitions = (int)(ceil(size_box/size_node));
+    size_box = 250;
+    partitions = 35;
+    size_node = size_box/(float)(partitions);
 
     //Allocate memory for the nodes depending of how many partitions there are.
     cucheck(cudaMalloc(&dnodeD_s1, partitions*partitions*partitions*sizeof(DNode)));
