f7cab3568e73f774e5047658d34f19c7b0cd1802
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-05 20:30:11 -0600

    Less restrictive BPC

--- cuda/2PCFisotropic/BPC/kernels.cuh
+++ cuda/2PCFisotropic/BPC/kernels.cuh
@@ -186,7 +186,7 @@ __global__ void make_histoXX(double *XX, PointW3D *elements, DNode *nodeD, int p
                     //count_distances12(XX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, nodeD[idx2].prev_i, nodeD[idx2].prev_i + nodeD[idx2].len, ds, dd_max, 2);
                 //}
 
-                con_z = ((nz1<=d_max_pm)&&(nz2>=front_pm))||((nz2<=d_max_pm)&&(nz1>=front_pm));
+                con_z = ((nz1<=d_max_pm+1)&&(nz2>=front_pm-1))||((nz2<=d_max_pm+1)&&(nz1>=front_pm-1));
                 if(con_z){
                     atomicAdd(&XX[1],1);
                     //histo_front_XX(SS,nodeX,dis_nod,0.0,0.0,fabs(dz_nod),false,false,con_z,row,col,mom,u,v,w);
@@ -208,8 +208,8 @@ __global__ void make_histoXX(double *XX, PointW3D *elements, DNode *nodeD, int p
                     //}
 
                     // Boundary node conditions:
-                    con_y = ((ny1<=d_max_pm)&&(ny2>=front_pm))||((ny2<=d_max_pm)&&(ny1>=front_pm));
-                    con_z = ((nz1<=d_max_pm)&&(nz2>=front_pm))||((nz2<=d_max_pm)&&(nz1>=front_pm));
+                    con_y = ((ny1<=d_max_pm+1)&&(ny2>=front_pm-1))||((ny2<=d_max_pm+1)&&(ny1>=front_pm-1));
+                    con_z = ((nz1<=d_max_pm+1)&&(nz2>=front_pm-1))||((nz2<=d_max_pm+1)&&(nz1>=front_pm-1));
                     if(con_y || con_z){
                         atomicAdd(&XX[2],1);
                         //histo_front_XX(SS,nodeX,dis_nod,0.0,sqrt(dy_nod),sqrt(dz_nod),false,con_y,con_z,row,col,mom,u,v,w);
@@ -235,9 +235,9 @@ __global__ void make_histoXX(double *XX, PointW3D *elements, DNode *nodeD, int p
                         //}
 
                         // Boundary node conditions:
-                        con_x = ((nx1<=d_max_pm)&&(nx2>=front_pm))||((nx2<=d_max_pm)&&(nx1>=front_pm));
-                        con_y = ((ny1<=d_max_pm)&&(ny2>=front_pm))||((ny2<=d_max_pm)&&(ny1>=front_pm));
-                        con_z = ((nz1<=d_max_pm)&&(nz2>=front_pm))||((nz2<=d_max_pm)&&(nz1>=front_pm));
+                        con_x = ((nx1<=d_max_pm+1)&&(nx2>=front_pm-1))||((nx2<=d_max_pm+1)&&(nx1>=front_pm-1));
+                        con_y = ((ny1<=d_max_pm+1)&&(ny2>=front_pm-1))||((ny2<=d_max_pm+1)&&(ny1>=front_pm-1));
+                        con_z = ((nz1<=d_max_pm+1)&&(nz2>=front_pm-1))||((nz2<=d_max_pm+1)&&(nz1>=front_pm-1));
                         if(con_x || con_y || con_z){
                             atomicAdd(&XX[3],1);
                             //histo_front_XX(SS,nodeX,dis_nod,sqrt(dx_nod),sqrt(dy_nod),sqrt(dz_nod),con_x,con_y,con_z,row,col,mom,u,v,w);
