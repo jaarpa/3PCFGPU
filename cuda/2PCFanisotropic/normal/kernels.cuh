2ebc5ca2b114c8514be7ff9dd098661de1956b30
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-06 17:05:33 -0600

    TEsting the inner loop

--- cuda/2PCFanisotropic/normal/kernels.cuh
+++ cuda/2PCFanisotropic/normal/kernels.cuh
@@ -177,6 +177,7 @@ __global__ void make_histoXX(double *XX, PointW3D *elements, DNode *nodeD, int p
             // Counts distances within the same node
             count_distances11(XX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, bn, ds, dd_max, 2);
             
+            /*
             int idx2, u=row,v=col,w=mom; // Position index of the second node
             float dx_nod12, dy_nod12, dz_nod12, dd_nod12; //Internodal distance
 
@@ -220,7 +221,7 @@ __global__ void make_histoXX(double *XX, PointW3D *elements, DNode *nodeD, int p
                     }
                 }
             }
-
+            */
         }
     }
 }
