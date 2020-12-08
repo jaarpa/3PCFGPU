db23c633893f0a8c05918faf4f7b12d8b24f0559
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 17:44:47 -0600

    First coppy in the symmetrization

--- cuda/3PCFisotropic/normal/kernels.cuh
+++ cuda/3PCFisotropic/normal/kernels.cuh
@@ -385,7 +385,7 @@ __global__ void make_histoXXX(double *XXX, PointW3D *elements, DNode *nodeD, int
 
 __global__ void simmetrization(double *s_XXX,double *XXX , int bn){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    s_XXX[i*bn*bn + i*bn + i] = XXX[i*bn*bn + i*bn + i];
+    for (int l=0; l<partitions*partitions*partitions) s_XXX[l] = XXX[l];
     double v;
     for (int j=i+1; j<bn; j++){
         for (int k=j+1; k<bn; k++){
