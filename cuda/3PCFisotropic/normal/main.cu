c40859e4b51646fcf99d56e15dc30903f94d5839
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 17:28:24 -0600

    I can access to nodes def before free maybe are the points

--- cuda/3PCFisotropic/normal/main.cu
+++ cuda/3PCFisotropic/normal/main.cu
@@ -276,8 +276,9 @@ int main(int argc, char **argv){
     /* ==========================  Free memory ===============================*/
     /* =======================================================================*/
 
-    cout << "Tot number of points in the copied host nodes " << hnodeD_s[partitions*partitions*partitions].end << endl;
-    
+    cout << "Tot number of points in the copied host nodes " << hnodeD_s[partitions*partitions*partitions-1].end << endl;
+    cout << "Tot number of points in the copied host nodes " << hnodeD_s[partitions*partitions*partitions-1].len << endl;
+
     //Free the memory
     cucheck(cudaStreamDestroy(streamDDD));
     cucheck(cudaStreamDestroy(streamDDR));
@@ -330,11 +331,11 @@ int main(int argc, char **argv){
     cucheck(cudaFree(dnodeR_DRR));
     cucheck(cudaFree(d_ordered_pointsR_DRR));
     
-    cout << "Free the host  D flattened nodes" << endl;
     delete[] hnodeD_s;
+    cout << "Free the host  D flattened nodes" << endl;
     delete[] h_ordered_pointsD_s;
-    cout << "Free the host  R flattened nodes" << endl;
     delete[] hnodeR_s;
+    cout << "Free the host  R flattened nodes" << endl;
     delete[] h_ordered_pointsR_s;
 
     cout << "Program terminated..." << endl;
