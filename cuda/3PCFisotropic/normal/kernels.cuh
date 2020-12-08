eb923a11af640cc7cf773ccd873c6e12a725a118
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 17:36:15 -0600

    Revert "Error at free"
    This reverts commit 822b5bb1eb090114bf215863b2a729ad743f9b44.

--- cuda/3PCFisotropic/normal/kernels.cuh
+++ cuda/3PCFisotropic/normal/kernels.cuh
@@ -305,6 +305,7 @@ __global__ void make_histoXXX(double *XXX, PointW3D *elements, DNode *nodeD, int
 
         if (nodeD[idx].len > 0){
 
+            printf("%i len: %i \n", idx, nodeD[idx].len);
             double t = (double)(nodeD[idx].len);
             atomicAdd(&XXX[0],t);
 
