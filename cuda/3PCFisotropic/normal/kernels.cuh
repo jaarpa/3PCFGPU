f571b4dec8fcab983d33304bf5ca8e413e896b0a
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 14:54:25 -0600

    Ommit the kernel launch and only symmetrizate to see if the data is being copied properly

--- cuda/3PCFisotropic/normal/kernels.cuh
+++ cuda/3PCFisotropic/normal/kernels.cuh
@@ -311,8 +311,7 @@ __global__ void make_histoXXX(double *XXX, PointW3D *elements, DNode *nodeD, int
             d_max_node*=d_max_node;
 
             // Counts distances within the same node
-            atomicAdd(&XXX[1241],1);
-            //count_111_triangles(XXX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, bn, ds, dd_max);
+            count_111_triangles(XXX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, bn, ds, dd_max);
             
             /*
             int idx2, u=row,v=col,w=mom; // Position index of the second node
