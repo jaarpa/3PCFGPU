6ab29ef2fb38a29674d00b201f51fe2c0d2ba8ec
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 16:21:14 -0600

    Count node id len > 0

--- cuda/3PCFisotropic/normal/3PCFiso.h
+++ cuda/3PCFisotropic/normal/3PCFiso.h
@@ -201,7 +201,9 @@ void NODE3P::make_histoXXX(double ***XXX, Node ***nodeX){
 	//==================================================
 	// Triangles between points of the same node:
 	//==================================================
-	*(*(*(XXX+(int)(0))+(int)(0))+(int)(0))+=1;
+	if (nodeX[row][col][mom].len>0){
+		*(*(*(XXX+(int)(0))+(int)(0))+(int)(0))+=1;
+	}
 	//count_3_N111(row, col, mom, SSS, nodeX);	
 	/*
 	//==================================================
