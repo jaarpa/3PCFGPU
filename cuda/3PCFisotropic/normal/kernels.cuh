c3b4e175f2c08cb3e5cc64e4eda80d70851909df
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-08 22:08:17 -0600

    Using 123 count

--- cuda/3PCFisotropic/normal/kernels.cuh
+++ cuda/3PCFisotropic/normal/kernels.cuh
@@ -5,136 +5,6 @@
 //============ Kernels Section ======================================= 
 //====================================================================
 
-__device__ void count_111_triangles(double *XXX, PointW3D *elements, int start, int end, int bns, float ds, float dd_max){
-    /*
-    This device function counts the triangles betweeen points within the same node. This function is used 
-    to compute the XXX histogram
-
-    Args:
-    XXX: (double*) The histogram where the triangles are counted in
-    elements: (PointW3D*)  Array of PointW3D points orderered coherently by the nodes
-    start: (int) index at which the nodeA starts to be defined by elements1. Inclusive.
-    end: (int) index at which the nodeA stops being defined by elements1. Non inclusive.
-    bns: (int) number of bins per XXX dimension
-    ds: (float) number of bins divided by the maximum distance. Used to calculate the bin it should be counted at
-    dd_max: (float) The maximum distance of interest.
-    sum: (int) State if each distance is counted twice or once
-    */
-    
-    int bin;
-    double v;
-    float x1,y1,z1,w1;
-    float x2,y2,z2,w2;
-    float x3,y3,z3;
-    float dd12, dd23, dd31;
-
-    for (int i=start; i<end-2; ++i){
-        x1 = elements[i].x;
-        y1 = elements[i].y;
-        z1 = elements[i].z;
-        w1 = elements[i].w;
-        for (int j=i+1; j<end-1; ++j){
-            x2 = elements[j].x;
-            y2 = elements[j].y;
-            z2 = elements[j].z;
-            w2 = elements[j].w;
-            dd12 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
-            if (dd12<dd_max){
-                for (int k=j+1; k<end; k++){
-                    x3 = elements[k].x;
-                    y3 = elements[k].y;
-                    z3 = elements[k].z;
-                    dd23 = (x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2);
-                    if (dd23<dd_max){
-                        dd31 = (x3-x1)*(x3-x1)+(y3-y1)*(y3-y1)+(z3-z1)*(z3-z1);
-                        if (dd31<dd_max){
-                            v = (double)(w1*w2*elements[k].w);
-                            bin = (int)(sqrtf(dd12)*ds)*bns*bns + (int)(sqrtf(dd23)*ds)*bns + (int)(sqrtf(dd31)*ds);
-                            atomicAdd(&XXX[bin],v);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-__device__ void count_112_triangles(double *XXX, PointW3D *elements, int start1, int end1, int start2, int end2, int bns, float ds, float dd_max){
-    /*
-    This device function counts the triangles betweeen 2 points in one node and the third point in a second node. Between two different nodes from the same file. 
-    This function is used to compute the XX histogram.
-
-    Args:
-    XXX: (double*) The histogram where the triangles are counted in
-    elements: (PointW3D*)  Array of PointW3D points orderered coherently by the nodes
-    start1: (int) index at which the nodeA starts to be defined by elements1. Inclusive.
-    end1: (int) index at which the nodeA stops being defined by elements1. Non inclusive.
-    start2: (int) index at which the nodeB starts to be defined by elements1. Inclusive.
-    end2: (int) index at which the nodeB stops being defined by elements1. Non inclusive.
-    bns: (int) number of bins per XXX dimension
-    ds: (float) number of bins divided by the maximum distance. Used to calculate the bin it should be counted at
-    dd_max: (float) The maximum distance of interest.
-    sum: (int) State if each distance is counted twice or once
-    */
-
-    int bin, bx, by, k;
-    double v;
-    float x1,y1,z1,w1;
-    float x2,y2,z2,w2;
-    float x3,y3,z3;
-    float dd12, dd23, dd31;
-
-    for (int i=start1; i<end1; ++i){
-        x1 = elements[i].x;
-        y1 = elements[i].y;
-        z1 = elements[i].z;
-        w1 = elements[i].w;
-        for (int j=start2; j<end2; ++j){
-            x2 = elements[j].x;
-            y2 = elements[j].y;
-            z2 = elements[j].z;
-            w2 = elements[j].w;
-            dd12 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
-            if (dd12<=dd_max){
-                bx = (int)(sqrtf(dd12)*ds)*bns*bns;
-                v = w1*w2;
-                for (k=i+1; k<end1; k++){
-                    x3 = elements[k].x;
-                    y3 = elements[k].y;
-                    z3 = elements[k].z;
-                    dd31 = (x3-x1)*(x3-x1)+(y3-y1)*(y3-y1)+(z3-z1)*(z3-z1);
-                    if (dd31<dd_max){
-                        by = (int)(sqrtf(dd31)*ds)*bns;
-                        dd23 = (x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2);
-                        if (dd23<dd_max){
-                            bin = bx + by + (int)(sqrtf(dd23)*ds);
-                            v *= elements[k].w;
-                            atomicAdd(&XXX[bin],v);
-
-                        }
-                    }
-                }
-                for (k=j+1; k<end2; k++){
-                    x3 = elements[k].x;
-                    y3 = elements[k].y;
-                    z3 = elements[k].z;
-                    dd31 = (x3-x1)*(x3-x1)+(y3-y1)*(y3-y1)+(z3-z1)*(z3-z1);
-                    if (dd31<dd_max){
-                        by = (int)(sqrtf(dd31)*ds)*bns;
-                        dd23 = (x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2);
-                        if (dd23<dd_max){
-                            bin = bx + by + (int)(sqrtf(dd23)*ds);
-                            v *= elements[k].w;
-                            atomicAdd(&XXX[bin],v);
-
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
 __device__ void count_123_triangles(double *XXX, PointW3D *elements, int start1, int end1, int start2, int end2, int start3, int end3, int bns, float ds, float dd_max){
     /*
     This device function counts the triangles betweeen points in three different nodes each. Between two different nodes from the same file. 
@@ -173,7 +43,7 @@ __device__ void count_123_triangles(double *XXX, PointW3D *elements, int start1,
             z2 = elements[j].z;
             w2 = elements[j].w;
             dd12 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
-            if (dd12<dd_max){
+            if (dd12>0 && dd12<dd_max){
                 bx = (int)(sqrtf(dd12)*ds)*bns*bns;
                 v = w1*w2;
                 for (int k=start3; k<end3; k++){
@@ -181,10 +51,10 @@ __device__ void count_123_triangles(double *XXX, PointW3D *elements, int start1,
                     y3 = elements[k].y;
                     z3 = elements[k].z;
                     dd23 = (x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2);
-                    if (dd23<dd_max){
+                    if (dd23>0 && dd23<dd_max){
                         by = (int)(sqrtf(dd23)*ds)*bns;
                         dd31 = (x3-x1)*(x3-x1)+(y3-y1)*(y3-y1)+(z3-z1)*(z3-z1);
-                        if (dd31<dd_max){
+                        if (dd31>0 && dd31<dd_max){
                             bin = bx + by + (int)(sqrtf(dd31)*ds);
                             v *= elements[k].w;
                             atomicAdd(&XXX[bin],v);
@@ -197,100 +67,22 @@ __device__ void count_123_triangles(double *XXX, PointW3D *elements, int start1,
 
 }
 
-    /*
-__device__ void inner_make_histoXXX(double *XXX, PointW3D *elements, DNode *nodeD, int idx, int idx2, int partitions, int bn, float ds, float d_max_node, float dd_max){
-
-    //Get the node positon of this thread
-    int col = (int) ((idx%(partitions*partitions))/partitions);
-    int row = idx%partitions;
-
-    //Get the node positon of this thread
-    int u = (int) (idx2/(partitions*partitions));
-    int v = (int) ((idx2%(partitions*partitions))/partitions);
-    int w = idx2%partitions;
-    
-    int idx3, a=u, b=v, c=w; //Position index of the third node
-
-    float nx1=nodeD[idx].nodepos.x, ny1=nodeD[idx].nodepos.y, nz1=nodeD[idx].nodepos.z;
-    float nx2=nodeD[idx2].nodepos.x, ny2=nodeD[idx2].nodepos.y, nz2=nodeD[idx2].nodepos.z;
-    float nx3=nx2, ny3=ny2, nz3;
-    float dx_nod31, dy_nod31, dz_nod31, dd_nod31, dd_nod23; //Internodal distance
-    float ddx31, ddy31, ddx23, ddy23;
-
-    ddx31 = (nx3 - nx1)*(nx3 - nx1);
-    ddy31 = (ny3 - ny1)*(ny3 - ny1);
-    //Third node mobil in Z direction
-    for (c=w+1;  c<partitions; ++c){
-        idx3 = row + col*partitions + c*partitions*partitions;
-        nz3 = nodeD[idx3].nodepos.z;
-        dz_nod31 = nz3-nz1;
-        dd_nod31 = dz_nod31*dz_nod31 + ddx31 + ddy31;
-        if (dd_nod31 <= d_max_node) {
-            dd_nod23 = (nz3-nz2)*(nz3-nz2);
-            if (dd_nod23 <= d_max_node) {
-                count_123_triangles(XXX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, nodeD[idx2].prev_i, nodeD[idx2].prev_i + nodeD[idx2].len, nodeD[idx2].prev_i, nodeD[idx3].prev_i + nodeD[idx3].len, bn, ds, dd_max);
-            }
-        }
-    }
-    //Third node mobil in YZ direction
-    for (b=v+1; b<partitions; ++b){
-        idx3 = row + b*partitions;
-        ny3 = nodeD[idx3].nodepos.y;
-        dy_nod31 = ny3-ny1;
-        for (c=0;  c<partitions; ++c){
-            idx3 = row + b*partitions + c*partitions*partitions;
-            nz3 = nodeD[idx3].nodepos.z;
-            dz_nod31 = nz3-nz1;
-            dd_nod31 = dy_nod31*dy_nod31 + dz_nod31*dz_nod31 + ddx31;
-            if (dd_nod31 <= d_max_node){
-                dd_nod23 = (ny3-ny2)*(ny3-ny2) + (nz3-nz2)*(nz3-nz2);
-                if (dd_nod23 <= d_max_node) {
-                    count_123_triangles(XXX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, nodeD[idx2].prev_i, nodeD[idx2].prev_i + nodeD[idx2].len, nodeD[idx2].prev_i, nodeD[idx3].prev_i + nodeD[idx3].len, bn, ds, dd_max);
-                }
-            }
-        }
-    }
-
-    //Third node mobil in YZ direction
-    for (a=u+1; a<partitions; ++a){
-        nx3 = nodeD[a].nodepos.x;
-        dx_nod31 = nx3 - nx1;
-        for (b=0; b<partitions; ++b){
-            idx3 = a + b*partitions;
-            ny3 = nodeD[idx3].nodepos.y;
-            dy_nod31 = ny3-ny1;
-            for (c=0;  c<partitions; ++c){
-                idx3 = a + b*partitions + c*partitions*partitions;
-                nz3 = nodeD[idx3].nodepos.z;
-                dz_nod31 = nz3-nz1;
-                dd_nod31 = dx_nod31*dx_nod31 + dy_nod31*dy_nod31 + dz_nod31*dz_nod31;
-                if (dd_nod31 <= d_max_node){
-                    dd_nod23 = (nx3-nx2)*(nx3-nx2) + (ny3 - ny2)*(ny3 - ny2) + (nz3 - nz2)*(nz3 - nz2);
-                    if (dd_nod23 <= d_max_node) {
-                        count_123_triangles(XXX, elements, nodeD[idx].prev_i, nodeD[idx].prev_i+nodeD[idx].len, nodeD[idx2].prev_i, nodeD[idx2].prev_i + nodeD[idx2].len, nodeD[idx2].prev_i, nodeD[idx3].prev_i + nodeD[idx3].len, bn, ds, dd_max);
-                    }
-                }
-            }
-        }
-    }
-
-}
-    */
 __global__ void make_histoXXX_child2(double *XXX, PointW3D *elements, DNode *nodeD, int idx1, int idx2, int partitions, int bn, float dmax, float size_node){
     int idx3 = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx3<(partitions*partitions*partitions)){
         if (nodeD[idx3].len > 0){
             float d_max_node = dmax + size_node*sqrtf(3.0);
             d_max_node*=d_max_node;
-            
+
             float nx1=nodeD[idx1].nodepos.x, ny1=nodeD[idx1].nodepos.y, nz1=nodeD[idx1].nodepos.z;
             float nx2=nodeD[idx2].nodepos.x, ny2=nodeD[idx2].nodepos.y, nz2=nodeD[idx2].nodepos.z;
             float nx3=nodeD[idx3].nodepos.x, ny3=nodeD[idx3].nodepos.y, nz3=nodeD[idx3].nodepos.z;
             float dd_nod23 = (nx3-nx2)*(nx3-nx2)+(ny3-ny2)*(ny3-ny2)+(nz3-nz2)*(nz3-nz2);
             float dd_nod31 = (nx3-nx1)*(nx3-nx1)+(ny3-ny1)*(ny3-ny1)+(nz3-nz1)*(nz3-nz1);
             if (dd_nod23<=d_max_node && dd_nod31<=d_max_node){
-                int bin = idx3%2;
-                atomicAdd(&XXX[bin],1.0);
+                float dd_max = dmax*dmax;
+                float ds = ((float)(bn))/dmax;
+                count_123_triangles(XXX, elements, nodeD[idx1].nodepos.start, nodeD[idx1].nodepos.end, nodeD[idx2].nodepos.start, nodeD[idx2].nodepos.end, nodeD[idx3].nodepos.start, nodeD[idx3].nodepos.end, bn, ds, dd_max);
             }
         }
     }
