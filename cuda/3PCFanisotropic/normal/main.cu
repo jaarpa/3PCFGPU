590b6ca8d4a92d34dd66f0c872b5d0821cca9919
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-09 15:56:06 -0600

    Count the points in the host

--- cuda/3PCFanisotropic/normal/main.cu
+++ cuda/3PCFanisotropic/normal/main.cu
@@ -258,7 +258,7 @@ int main(int argc, char **argv){
     /* =======================================================================*/
     double s = 0;
     for (int i=0; i<bn*bn*bn*bn*bn; i++){
-        s+=DDD[i]
+        s+=DDD[i];
     }
     cout << "the total number of counts:" <<  s << endl;
 	save_histogram(nameDDD, bn, DDD);
