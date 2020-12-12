18af7a02d1da9e66d418439c387f74391fb9598c
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-11 23:07:22 -0600

    Reorganization of the files

--- cpp/CUDA/3PCFisotropica/check.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-DDD_gpu = np.loadtxt("DDD_GPU.dat")
-DDD_cpu = np.loadtxt("DDD_CPU.dat")
-
-#DDD_gpu = np.loadtxt("DDD.res")
-#DDD_cpu = np.loadtxt("DDDiso_mesh_3D_rand0_5K.dat.dat")
-
-print((DDD_gpu==DDD_cpu).all())
