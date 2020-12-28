0efe7ee6f490c815d8e5d074c6ea91da4ffdfe1c
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-27 19:19:52 -0600

    Syntax error

--- cuda/3PCFisotropic/BPC/main.cu
+++ cuda/3PCFisotropic/BPC/main.cu
@@ -142,7 +142,7 @@ int main(int argc, char **argv){
     cucheck(cudaMemsetAsync(d_DD_ff_av_ref, 0, bn_XX_ff_av_ref*sizeof(double), stream_analytic));
     cucheck(cudaMemsetAsync(d_RR_ff_av, 0, bn_XX_ff_av*sizeof(double), streamRR_ff_av));
     cucheck(cudaMemsetAsync(d_RR_ff_av_ref, 0, bn_XX_ff_av_ref*sizeof(double), streamRR_ff_av_ref));
-    cucheck(cudaMemsetAsync(d_ff_av, 0, bn*sizeof(double), streamRR_ff_av);
+    cucheck(cudaMemsetAsync(d_ff_av, 0, bn*sizeof(double), streamRR_ff_av));
 
     hnodeD = new Node**[partitions];
     for (int i=0; i<partitions; i++){
