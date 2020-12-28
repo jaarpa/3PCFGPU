43d4cb10d1286edaff22a0f082b21149eb635449
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-28 01:26:53 -0600

    Compare the first 3 items of ff_av and ff_Av_ref

--- cuda/3PCFisotropic/BPC/kernels.cuh
+++ cuda/3PCFisotropic/BPC/kernels.cuh
@@ -1140,8 +1140,8 @@ __global__ void make_histo_analitic(double *XXY, double *RRR, double *ff_av, dou
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int k = blockIdx.z * blockDim.z + threadIdx.z;
     if (i==0 && j==0 && k ==0){
-        printf("ff_av_ref: %f, %f, %f", ff_av_ref[0], ff_av_ref[1], ff_av_ref[2] );
-        printf("ff_av: %f, %f, %f", ff_av[0], ff_av[1], ff_av[2] );
+        printf("ff_av_ref: %f, %f, %f \n", ff_av_ref[0], ff_av_ref[1], ff_av_ref[2] );
+        printf("ff_av: %f, %f, %f \n", ff_av[0], ff_av[1], ff_av[2] );
     }
     if (i<bn && j<bn && k<bn){
 
