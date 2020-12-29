faf48e666cb56c947d47ff38b83a39c86d4acbfc
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-12-28 19:24:27 -0600

    Removed DD and RR prints, now they are being calculated properly. Test analytic histo again

--- cuda/3PCFisotropic/BPC/kernels.cuh
+++ cuda/3PCFisotropic/BPC/kernels.cuh
@@ -1101,13 +1101,6 @@ __global__ void make_ff_av(double *ff_av, double *XX, double *YY, float dmax, in
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     
-    if (i==0 && j==0){
-        printf("DD_ff_av \n");
-        for(int a=2989; a<3000; a++){
-            printf("%f \n", XX[a]);
-        }
-    }
-    
     if (i<bn && j<ptt){
         int i_ = i*ptt;
         double ri = i*dmax/(double)bn;
