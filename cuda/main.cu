1951f0de26b483fd52f421273220e9bad4d9d316
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2021-01-03 18:12:29 -0600

    Print a couple of var to check them

--- cuda/main.cu
+++ cuda/main.cu
@@ -181,7 +181,7 @@ if (rand_dir){
         
         cout << "Properly assigned and read the data. Justbefore making the nodes" << endl;
         cout << size_box << endl;
-        cout << dataR[i][5].z << endl;
+        cout << dataR[0][5].z << endl;
 
         make_nodos(hnodeD, dataD, partitions, size_node, np);
         if (rand_required){
