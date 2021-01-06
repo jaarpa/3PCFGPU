171dc19c74a2b67d9442f3a0ace1b9da8a07c974
Josue De Santiago <joselotl@gmail.com>
2021-01-06 13:23:29 -0600

    Variate partitions in [10,20] step 2

--- cuda/3PCF_param.sh
+++ cuda/3PCF_param.sh
@@ -7,35 +7,35 @@
 #N_bins --> 20
 
 
-#touch time_results.dat #Abrimos archivo
+touch time_results.dat #Abrimos archivo
 nvcc -arch=sm_75 3PCFisotropic/normal/optimization.cu -o 3PCFisotropic/normal/opt.out
 
 # ------------------- Encabezado del archivo ----------------------------------------
 
-#echo "3PCF isotropica. Sin BPC. Histograma DDD \n" >> time_results.dat
-#echo "caja 512MPc \n" >> time_results.dat
-#echo "n_points, size_box, d_max, partitions, time [s] \n" >> time_results.dat
+echo "3PCF isotropica. Sin BPC. Histograma DDD \n" >> time_results.dat
+echo "caja 512MPc \n" >> time_results.dat
+echo "n_points, size_box, d_max, partitions, time [s] \n" >> time_results.dat
 
 # ------------------- Cuerpo del archivo --------------------------------------------
 
 # ------------------- Para caja de 512MPc -------------------------------------------
 
-#for dmax in $(seq 20 10 60)
-#do 
-	#for partitions in $(seq 20 5 60)
-	#do
-			#3PCFisotropic/normal/opt.out data_512MPc.dat data_512MPc.dat 50653 20 $dmax -1 $partitions
-	#done	
-#done
+for dmax in $(seq 20 10 60)
+do 
+	for partitions in $(seq 10 2 20)
+	do
+		3PCFisotropic/normal/opt.out data_512MPc.dat data_512MPc.dat 50653 20 $dmax -1 $partitions
+	done	
+done
 
 
 # ------------------- Para caja de 1GPc --------------------------------------------
 
 for dmax in $(seq 30 10 60)
 do 
-	for partitions in $(seq 15 5 45)
+	for partitions in $(seq 10 2 20)
 	do
-			3PCFisotropic/normal/opt.out data_1GPc.dat data_1GPc.dat 405224 20 $dmax -1 $partitions
+		3PCFisotropic/normal/opt.out data_1GPc.dat data_1GPc.dat 405224 20 $dmax -1 $partitions
 	done	
 done
 
@@ -44,7 +44,7 @@ done
 
 for dmax in $(seq 20 10 60)
 do 
-	for partitions in in $(seq 15 5 45)
+	for partitions in in $(seq 10 2 20)
 	do
 			3PCFisotropic/normal/opt.out data_2GPc.dat data_2GPc.dat 3241792 20 $dmax -1 $partitions
 	done	
