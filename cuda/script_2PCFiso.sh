cc817f2f55d44fdd8d7126136023b24355755603
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-07-07 02:00:38 -0500

    Act: Ready to run 2GPc box

--- cuda/script_2PCFiso.sh
+++ cuda/script_2PCFiso.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 #=================================================================================================
-# Script para correr de una sola vez todas las realizaciones de las cajas de 1 GPc
-# Todas estas cajas contienen 405224 puntos y se correran a dmax = 140 MPc para 30 bins
+# Script para correr de una sola vez todas las realizaciones de las cajas de 2 GPc
+# Todas estas cajas contienen 3241792 puntos y se correran a dmax = 140 MPc para 30 bins
 #================================================================================================
 
-for i in $(seq 0 1 9)
+for i in $(seq 0 1 4)
 do
-	./PCF.out 2iso -f data_1GPc_$i.dat -r rand0_1GPc_$i.dat -n 405224 -b 30 -d 140
+	./PCF.out 2iso -f data_2GPc_$i.dat -r rand0_2GPc_$i.dat -n 3241792 -b 30 -d 140
 done
 
 echo "===================================================="
