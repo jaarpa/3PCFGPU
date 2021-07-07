dc95e34bbd2d0b2688761f25cde495237efed661
Josue De Santiago <joselotl@gmail.com>
2021-07-06 22:53:44 -0500

    Script de bash para evaluacion del tiempo de computo del histograma DDD de 3PCFiso

---
+++ cuda/script_for_time_results.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#=================================================================================================
+# Script para analizar el tiempo de computo del histograma DDD de la 3PCF isotropica sin BPC
+# para la caja 0 de 1 GPc 
+#================================================================================================
+
+for dmax in $(seq 30 10 180)
+do
+	./PCF.out 3iso -f data_1GPc_0.dat -r data_1GPc_0.dat -n 405224 -b 30 -d $dmax
+done
+
+echo "===================================================="
+echo " Analisis terminado"
+echo "===================================================="
