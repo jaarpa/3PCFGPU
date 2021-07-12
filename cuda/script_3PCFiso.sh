88ec64eeb67bf6029597ad4a26dd9325ba6137ec
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-07-12 14:46:13 -0500

    Script ready to run 3PCF iso 1 GPc

--- cuda/script_3PCFiso.sh
+++ cuda/script_3PCFiso.sh
@@ -5,9 +5,9 @@
 # Todas estas cajas contienen 50653 puntos y se correran a dmax = 140 MPc para 30 bins
 #================================================================================================
 
-for i in $(seq 0 1 14)
+for i in $(seq 0 1 9)
 do
-	./PCF.out 3iso -f data_512MPc_$i.dat -r rand0_512MPc_$i.dat -n 50653 -b 30 -d 140
+	./PCF.out 3iso -f data_1GPc_$i.dat -r rand0_1GPc_$i.dat -n 405224 -b 30 -d 140
 done
 
 echo "===================================================="
