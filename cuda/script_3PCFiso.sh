feb1eb050d2f59b889cf96d0850ce94b756c708b
Josue De Santiago <joselotl@gmail.com>
2021-07-17 11:51:05 -0500

    Script ready to run 3PCF iso 512MPc

--- cuda/script_3PCFiso.sh
+++ cuda/script_3PCFiso.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 #=================================================================================================
-# Script para correr de una sola vez todas las realizaciones de las cajas de 1 GPc
-# Todas estas cajas contienen 405224 puntos y se correran a dmax = 140 MPc para 30 bins
+# Script para correr de una sola vez todas las realizaciones de las cajas de 512MPc
+# Todas estas cajas contienen 50653 puntos y se correran a dmax = 140 MPc para 30 bins
 #================================================================================================
 
-for i in $(seq 5 1 9)
+for i in $(seq 0 1 9)
 do
-	./PCF.out 3iso -f data_1GPc_$i.dat -r rand0_1GPc_$i.dat -n 405224 -b 30 -d 140
+	./PCF.out 3iso -f data_512Pc_$i.dat -r rand0_512MPc_$i.dat -n 50653 -b 30 -d 140
 done
 
 echo "===================================================="
